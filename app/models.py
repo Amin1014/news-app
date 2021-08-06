71b00fa6d57d5c54f5748eb6137d4b74f84b8eb4
Amin1014 <aminabdi570@gmail.com>
2021-08-06 14:39:52 +0300

    adding different classes for users

---
+++ app/models.py
@@ -0,0 +1,26 @@
+class Source:
+  '''
+  Source class to define Movie objects
+  '''
+  def __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+    self.source = source
+    self.author= author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.urlToImage = urlToImage
+    self.publishedAt = publishedAt
+
+class Article:
+  '''
+  Article class to define an artivle object
+  '''
+
+  def __init__(self,source,author,title,description,url, urlToImage,publishedAt):
+    self.source = source
+    self.author = author
+    self.title = title
+    self.description = description
+    self.urlToImage = urlToImage
+    self.url = url
+    self.publishedAt = publishedAt
