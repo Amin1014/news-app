2d2a4481a4dc1d4fe107439628c2ca259a4952cb
Amin1014 <aminabdi570@gmail.com>
2021-08-08 10:09:46 +0300

    add all changes"

---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY='398f05cdb8a24e9a93f32ef882755742'
+export SECRET_KEY='012345'
+
+python3.9 manage.py server
---
+++ tests/news_test.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import Source
+
+class NewsTest(unittest.TestCase):
+  '''
+  Test Class to test the behaviour of the news class
+  '''
+  def setUp(self):
+    '''
+    A setup function that will run before any test
+    '''
+    self.new_news = News('ABC News', 'Your trusted source for breaking news', 'https://abcnews.go.com',
+    'general', 'en','country')
+
+  def test_init(self):
+    '''
+    Tests whether the news object is instantiated correctly
+    '''
+    self.assertTrue(instance(self.new_news, Source))
